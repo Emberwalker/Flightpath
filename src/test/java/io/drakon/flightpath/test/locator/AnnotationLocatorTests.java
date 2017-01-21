de6212bf7e9a64278541088d50bc2ec419f6655b
Arkan Emberwalker <arkan@drakon.io>
2017-01-21 09:36:24 +0000

    Start implementing tests.

---
+++ src/test/java/io/drakon/flightpath/test/locator/AnnotationLocatorTests.java
@@ -0,0 +1,36 @@
+package io.drakon.flightpath.test.locator;
+
+import io.drakon.flightpath.Airdrop;
+import io.drakon.flightpath.lib.AnnotationLocator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
+
+public class AnnotationLocatorTests {
+
+    @Test
+    public void testLocatorSingleClass() throws NoSuchMethodException {
+        AnnotationTestClass testClass = new AnnotationTestClass();
+        Map<Class, Set<Method>> ret = new AnnotationLocator(Airdrop.class).findSubscribers(testClass);
+
+        assertTrue(ret.containsKey(Object.class) && ret.containsKey(DummyEvent.class));
+
+        Method one = AnnotationTestClass.class.getDeclaredMethod("one", Object.class);
+        Method two = AnnotationTestClass.class.getDeclaredMethod("two", DummyEvent.class);
+        assertTrue(ret.get(Object.class).size() == 1 && ret.get(Object.class).contains(one));
+        assertTrue(ret.get(DummyEvent.class).size() == 1 && ret.get(DummyEvent.class).contains(two));
+    }
+
+    private class AnnotationTestClass {
+        @Airdrop
+        public void one(Object event) {}
+
+        @Airdrop
+        public void two(DummyEvent event) {}
+    }
+
+}
---
+++ src/test/java/io/drakon/flightpath/test/locator/DummyEvent.java
@@ -0,0 +1,6 @@
+package io.drakon.flightpath.test.locator;
+
+/**
+ * Just a placeholder class for an example event type.
+ */
+public class DummyEvent {}
